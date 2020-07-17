d43e3f6920162fec4c2c63b290a38cc3add714e1
Tesla <547249523@qq.com>
2020-07-17 09:41:55 +0800

    chore: 修改favicon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="main-app">
-        <router-link to="/admall/login">v2</router-link>
-        <router-view v-show="$route.name" />
+        <section v-show="$route.name">
+            <router-link to="/admall/login">v2</router-link>
+            <router-view />
+        </section>
         <!-- 子应用渲染区 -->
         <section id="child-app" v-show="!$route.name" />
     </div>
