5576d851eebc0714c9fcd83475e0f67921a762f6
Tesla <547249523@qq.com>
2020-09-07 16:57:05 +0800

    feat: header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,14 +1,32 @@
 <template>
     <header :class="$style.header">
         <div :class="$style.left">
-
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+                <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
         </div>
         <div :class="$style.right">
             <div :class="$style.notice">
-                通知中心
+                <pl-svg
+                    :class="$style.icon"
+                    name="icon-tongzhi-bfcfa"
+                    width="15"
+                    height="18"
+                />
+                <span>通知中心</span>
+                <i :class="$style.mark" />
             </div>
-            <div>
-
+            <div :class="$style.user">
+                <img
+                    :class="$style.avatar"
+                    src="https://mallcdn.youpenglai.com/static/admall-new/3.0.0/yonghu.png"
+                    alt="avatar"
+                >
+                <span :class="$style.mobile">133****3333</span>
+                <span class="el-icon-arrow-right fz-12" />
             </div>
         </div>
     </header>
@@ -26,9 +44,48 @@ export default class Header extends Vue {
 
 <style module lang="scss">
     .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         height: 60px;
-        font-size: 30px;
+        padding: 0 24px;
         background-color: #fff;
         box-shadow: 0 1px 0 #E7E7E7;
+    }
+    .right {
+        display: inline-flex;
+        align-items: center;
+        .notice {
+            position: relative;
+            display: inline-flex;
+            align-items: center;
+            > .icon {
+                margin-right: 7px;
+            }
+            > .mark {
+                position: absolute;
+                top: -2px;
+                right: -3px;
+                width: 4px;
+                height: 4px;
+                background-color: $--color-red-1;
+                border-radius: 2px;
+            }
+        }
+        .user {
+            display: inline-flex;
+            align-items: center;
+            margin-left: 20px;
+            padding-left: 20px;
+            border-left: 1px solid $--color-gray-5;
+        }
+        .avatar {
+            width: 20px;
+            height: 20px;
+            margin-right: 4px;
+        }
+        .mobile {
+            font-size: 14px;
+        }
     }
 </style>
--- src/components/common/Main-Navbar.vue
+++ src/components/common/Main-Navbar.vue
@@ -116,24 +116,6 @@
                 <el-menu-item index="7-4">物流设置</el-menu-item>
                 <el-menu-item index="7-5">我订购的服务</el-menu-item>
             </el-submenu>
-            <!--<el-menu-item index="2">财务</el-menu-item>
-            <el-submenu index="1">
-                <template slot="title">
-                    <span>导航一</span>
-                </template>
-                <el-menu-item-group>
-                    <template slot="title">分组一</template>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
-            </el-submenu>-->
         </el-menu>
     </nav>
 </template>
