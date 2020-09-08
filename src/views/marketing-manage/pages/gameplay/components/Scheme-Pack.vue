1f7d58daf163b36e0fa9d32bc2fd83efc80f07ff
lishaobos <www.long126@qq.com>
2020-09-08 15:38:30 +0800

    fix: 营销方案图标

--- src/views/marketing-manage/pages/gameplay/components/Scheme-Pack.vue
+++ src/views/marketing-manage/pages/gameplay/components/Scheme-Pack.vue
@@ -14,6 +14,14 @@
             <div>有效期：{{ expired }}</div>
             <div>当前活动：{{ count }}个</div>
         </div>
+        <div
+            :class="{
+                [$style.lock]: true,
+                [$style.unlock]: !isLock
+            }"
+        >
+            <img :src="isLock ? 'https://mallcdn.youpenglai.com/static/admall-new/3.0.0/lock.png' : 'https://mallcdn.youpenglai.com/static/admall-new/3.0.0/unlock.png'" alt="">
+        </div>
     </div>
 </template>
 
@@ -39,8 +47,13 @@ export default class SchemePack extends Vue {
 
     @Prop({
         type: String,
-        default: ''
+        default: '长期有效'
     }) readonly expired: string | undefined
+
+    @Prop({
+        type: Boolean,
+        default: false
+    }) readonly isLock: boolean | undefined
 
     @Prop({
         type: [String, Object],
@@ -52,7 +65,7 @@ export default class SchemePack extends Vue {
         default: 0
     }) readonly count: string | number | undefined
 
-    target () {
+    private target () {
         if (typeof this.routeInfo === 'string') {
             return this.$router.push({ name: this.routeInfo })
         }
@@ -64,6 +77,7 @@ export default class SchemePack extends Vue {
 <style module lang="scss">
 
 .scheme-pack {
+    position: relative;
     display: inline-block;
     border-radius: 10px;
     background-color: #F5F6FA;
@@ -76,6 +90,7 @@ export default class SchemePack extends Vue {
     > .content {
         display: flex;
         box-sizing: border-box;
+        height: 88px;
         padding: 20px;
         > .img {
             width: 48px;
@@ -88,21 +103,23 @@ export default class SchemePack extends Vue {
             align-content: center;
             margin-left: 10px;
             > .title {
+                display: flex;
+                align-items: flex-start;
                 width: 100%;
                 > .name {
                     margin-bottom: 4px;
-                    vertical-align: bottom;
                     font-size: 14px;
                     color: #333333;
                 }
                 > .tab {
                     display: inline-block;
+                    width: 16px;
                     line-height: 14px;
-                    padding: 3px;
                     box-sizing: border-box;
                     margin-left: 6px;
                     border: 1px solid #F79F1A;
                     border-radius: 2px;
+                    text-align: center;
                     font-size: 8px;
                     color: #F79F1A;
                 }
@@ -121,6 +138,19 @@ export default class SchemePack extends Vue {
         padding: 10px 20px;
         font-size: 10px;
         color: #6E7B8E;
+    }
+    > .lock {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 20px;
+        height: 20px;
+        border-radius: 0px 10px 0px 10px;
+        text-align: center;
+        background-color: #DCDDE0;
+        &.unlock {
+            background-color: #fff;
+        }
     }
 }
 
