f60b5ebab8bfaa412e122e70a3ac55cf953bf05d
lishaobos <www.long126@qq.com>
2020-09-14 11:39:38 +0800

    fix: 基础设置路由

--- src/router/modules/base-setting/account.ts
+++ src/router/modules/base-setting/account.ts
@@ -1,16 +1,19 @@
 export default [
     {
-        path: 'account-manage',
-        redirect: '/base-setting/account-manage/manage',
+        path: '/base-setting/account-manage/manage',
+        redirect: '/base-setting/account-manage/manage/list',
         name: 'AccountManage',
         component: () => import('../../../views/base-setting/account-manage/Index.vue'),
+        meta: {
+            title: '基础设置'
+        },
         children: [
             {
                 path: 'list',
                 name: 'AccountList',
                 component: () => import('../../../views/base-setting/account-manage/pages/Account-List.vue'),
                 meta: {
-                    title: '账号列表'
+                    title: '账号管理'
                 }
             }
         ]
