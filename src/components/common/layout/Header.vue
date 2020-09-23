142fcd6bd6c34ec11607f70efa632e823ae83888
Tesla <547249523@qq.com>
2020-09-23 10:12:32 +0800

    fix: 访问店铺弹框

--- src/components/common/layout/Header.vue
+++ src/components/common/layout/Header.vue
@@ -27,6 +27,29 @@
             </el-breadcrumb>
         </div>
         <div :class="$style.right">
+            <!-- 访问店铺 -->
+            <div :class="$style.visitShop">
+                <img @click="showMallUrl = true" width="15" class="pointer" src="https://mallcdn.youpenglai.com/static/admall-new/3.0.0/shangchengguanli.png" alt="">
+                <span @click="showMallUrl = true" class="pointer">访问店铺</span>
+
+                <transition name="fade">
+                    <div v-show="showMallUrl" :class="$style.visitShopBox">
+                        <i class="el-icon-close" @click="showMallUrl = false" />
+                        <div :class="$style.title">访问店铺</div>
+                        <div :class="$style.content">
+                            <div style="text-align: center">H5店铺</div>
+                            <div :class="$style.shopUrl">
+                                <div :class="$style.input" v-text="mallUrl" />
+                                <button v-clipboard:copy="mallUrl" v-clipboard:success="onCopy" v-clipboard:error="onError">复制</button>
+                            </div>
+                            <img :src="mallQrcode" alt="">
+                            <div style="text-align: center">手机扫描可直接查看店铺</div>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+
+            <!-- 通知中心按钮 -->
             <router-link
                 tag="div"
                 :class="$style.notice"
@@ -87,17 +110,20 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Component from 'vue-class-component'
+import { Component, Watch } from 'vue-property-decorator'
 import { namespace, Getter } from 'vuex-class'
+import { generateQrcode } from '../../../assets/ts/utils'
 const userModule = namespace('user')
-// import actions from '../../micro/shared/actions'
 
 @Component
 export default class Header extends Vue {
+    mallQrcode = ''
+    showMallUrl = false
     // computed
     @userModule.Getter('mallName') mallName!: string
     @userModule.Getter('bindPhone') bindPhone!: string
     @userModule.Getter('auditStatus') auditStatus!: string
+    @userModule.Getter('mallUrl') mallUrl!: string
     @Getter('childRoute') childRoute!: any
 
     @userModule.Mutation('LOGOUT') LOGOUT!: Function
@@ -110,11 +136,26 @@ export default class Header extends Vue {
         if (command === 'modify') return this.$router.push({ name: 'ModifyPassword' })
     }
 
-    // mounted () {
-    //     actions.onGlobalStateChange(state => {
-    //         console.log(state)
-    //     })
-    // }
+    // watch
+    @Watch('mallUrl', { immediate: true })
+    async onMallUrlChange (val: string) {
+        if (val) {
+            this.mallQrcode = await generateQrcode({
+                text: val
+            })
+        }
+    }
+
+    // methods
+    // 复制成功
+    onCopy () {
+        this.$success('复制成功')
+    }
+
+    // 复制失败
+    onError () {
+        this.$warning('复制失败')
+    }
 }
 </script>
 
@@ -171,5 +212,71 @@ export default class Header extends Vue {
             }
         }
     }
-
+    .visit-shop {
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+        margin-right: 32px;
+        > img {
+            margin-right: 6px;
+        }
+    }
+    .visit-shop-box {
+        position: absolute;
+        top: 40px;
+        right: 0;
+        width: 376px;
+        height: 436px;
+        background-color: #fff;
+        box-shadow: 0 3px 5px rgba(0, 0, 0, 0.05);
+        border-radius: 2px;
+        z-index: 10;
+        > i {
+            top: 25px;
+            right: 25px;
+            font-size: 16px;
+            cursor: pointer;
+            position: absolute;
+        }
+        .title {
+            line-height: 65px;
+            padding: 0 36px;
+            font-size: 16px;
+            font-weight: bold;
+            border-bottom: 1px solid $--border-color;
+        }
+        .content {
+            padding-top: 40px;
+            > img {
+                display: block;
+                width: 160px;
+                margin: 27px auto 13px;
+                padding: 10px;
+                border: 1px dashed $--border-color;
+            }
+        }
+        .shop-url {
+            display: flex;
+            align-items: center;
+            width: 280px;
+            margin: 20px auto 0;
+            text-align: center;
+            > .input {
+                flex: 1;
+                padding: 0 10px;
+                line-height: 30px;
+                color: #D4D4D4;
+                border: 1px solid $--border-color;
+                background-color: #F5F5F5;
+                @include elps-wrap(1);
+            }
+            > button {
+                width: 50px;
+                line-height: 32px;
+                color: #fff;
+                border: none;
+                background-color: $--color-primary-blue;
+            }
+        }
+    }
 </style>
