bf6c49f9fbb2b64f31417bfe6f34e22ecb5c0152
matthew <527692503@qq.com>
2020-12-07 15:08:05 +0800

    fix: #2426

--- src/views/marketing-manage/playing-methods/red-package/Add-Red-Package.vue
+++ src/views/marketing-manage/playing-methods/red-package/Add-Red-Package.vue
@@ -712,6 +712,7 @@ export default class AddRedPackage extends Vue {
                 data.redPacketCouponDTO.useWithCoupon = 0
                 data.redPacketCouponDTO.scholarship = 0
             }
+            if (data.redPacketCouponDTO.activityLimit) data.quantityLimit = ''
             if (!data.redPacketCouponDTO.receiveLimit) data.redPacketCouponDTO.tagIds = []
             this.id && this.$route.name === 'EditRedPackage' ? await editRedPackage(this.id, data) : await addRedPackage(data)
             this.$router.replace({ name: 'RedPackage' })
