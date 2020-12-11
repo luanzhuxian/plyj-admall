ce9c0715416664db1f855279cef496ce58523413
matthew <527692503@qq.com>
2020-12-11 11:09:51 +0800

    fix: 抽奖乐翻天营销中心入口及购买,预购优化

--- src/views/marketing-manage/playing-methods/happy-lottery/Index.vue
+++ src/views/marketing-manage/playing-methods/happy-lottery/Index.vue
@@ -1,32 +1,41 @@
 <template>
-    <div class="wrap">
-        <router-view />
+    <div>
+        <router-view v-if="loading" :info="info" />
     </div>
 </template>
 
-<script>
-import { MutationTypes } from '../../../../store/mutation-type'
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { namespace } from 'vuex-class'
+import { MutationTypes } from '@/store/mutation-type'
 import moment from 'moment'
-import { mapActions, mapGetters } from 'vuex'
 
-export default {
-    name: 'HappyLottery',
-    async created () {
-        if (!this.marketStatusAuth || !this.marketStatusAuth.length) await this[MutationTypes.getMarketStatusAuth]()
-        if (!this.marketStatusAuth || !this.marketStatusAuth.length) return
-        const info = this.marketStatusAuth.find(({ programId }) => programId === '7')
-        if (!info || moment(info.validity).valueOf() < Date.now()) {
-            await this.$router.replace({ name: 'MarketingGameplayList' })
-            await this.$alert('龙门节专属活动，若要开通请联系城市经理或者客服')
+const account = namespace('account')
+
+    @Component
+export default class HappyLotteryIndex extends Vue {
+        @account.Getter marketStatusAuth!: any[]
+        @account.Action(MutationTypes.getMarketStatusAuth) getMarketStatusAuth!: Function
+
+        loading = false
+        programId = '10'
+        info = {}
+
+        async created () {
+            try {
+                if (!this.marketStatusAuth || !this.marketStatusAuth.length) await this.getMarketStatusAuth()
+                this.loading = true
+                const info: any = this.marketStatusAuth.find(({ programId }) => programId === this.programId)
+                this.info = info
+                if (!info || moment(info.validity).valueOf() < Date.now()) {
+                    this.$router.replace({ name: 'MarketingUnpaidDetail', params: { programId: this.programId } })
+                }
+            } catch (e) {
+                throw e
+            }
         }
-    },
-    computed: {
-        ...mapGetters({
-            marketStatusAuth: 'account/marketStatusAuth'
-        })
-    },
-    methods: {
-        ...mapActions('account', [MutationTypes.getMarketStatusAuth])
-    }
 }
 </script>
+
+<style lang="scss">
+</style>
